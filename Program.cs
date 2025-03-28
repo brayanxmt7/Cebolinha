395ee53892c437360fb56acfd9e793c9c85dd93e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 19:30:10 -0300

    resolvido

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿﻿string frase;
+
+Console.WriteLine("Escreva uma frase como se ela fosse dita pelo Cebolinha, da Turma da Mônica");
+Console.WriteLine();
+Console.WriteLine("Exemplo: Voce merece aprausos, so que na cara ");
+frase = Console.ReadLine();
+
+
+
+frase = frase
+        .Replace("r", "l")
+        .Replace("R", "L");
+
+Console.WriteLine(frase);
